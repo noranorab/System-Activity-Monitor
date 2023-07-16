cda2c6ec60acef79af80519a9ecb30989b4dd2c2
noranorab <noranorab7@gmail.com>
2023-07-16 19:14:18 +0100

    implement format system

---
+++ src/system.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <cstddef>
+#include <set>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+#include "linux_parser.h"
+#include "process.h"
+#include "processor.h"
+#include "system.h"
+
+using std::set;
+using std::size_t;
+using std::string;
+using std::vector;
+
+// Return the system's CPU
+Processor &System::Cpu() { return cpu_; }
+
+// Return a container composed of the system's processes
+vector<Process> &System::Processes() {
+  // Get current pids
+  processes_.clear();
+  vector<int> pids = LinuxParser::Pids();
+  for (unsigned int i = 0; i < pids.size(); i++) {
+    Process p = Process(pids[i]);
+    processes_.push_back(p);
+  }
+  // Sort processes by cpu usage
+  std::sort(processes_.begin(), processes_.end());
+  std::reverse(processes_.begin(), processes_.end());
+  return processes_;
+}
+
+// Return the system's kernel identifier (string)
+std::string System::Kernel() { return LinuxParser::Kernel(); }
+
+// Return the system's memory utilization
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
+
+// Return the operating system name
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
+
+// Return the number of processes actively running on the system
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
+
+// Return the total number of processes on the system
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
+
+// Return the number of seconds since the system started running
+long int System::UpTime() { return LinuxParser::UpTime(); }
+
+// Return disk available devices since the system started running
+std::vector<std::string> System::GetAvailableDevices() { return LinuxParser::GetAvailableDevices(); }
+
+// Return disk available partitions since the system started running
+std::vector<std::string> System::GetAvailablePartitions() { return LinuxParser::GetAvailablePartitions(); }
+
