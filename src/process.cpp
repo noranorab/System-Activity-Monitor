3e9c5bb160e1565aaadd824f82bee84c1e35eeed
noranorab <noranorab7@gmail.com>
2023-07-16 16:56:15 +0100

    implement process processor

---
+++ src/process.cpp
@@ -0,0 +1,74 @@
+#include <cctype>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+#include "linux_parser.h"
+#include "process.h"
+
+using std::string;
+using std::to_string;
+using std::vector;
+
+// Constructor
+Process::Process(int PID) {
+  this->pid = PID;
+  updateCpuUtilization();
+}
+
+// Return this process's ID
+int Process::Pid() const { return this->pid; }
+
+// Return this process's CPU utilization
+float Process::getCpuUtilization() const { return cpu_utilization; }
+
+void Process::updateCpuUtilization() {
+
+  // Get raw data
+  vector<string> cpu_usage = LinuxParser::ProcessCpuUtilization(this->pid);
+  const long int utime = std::stol(cpu_usage[0]);
+  const long int stime = std::stol(cpu_usage[1]);
+  const long int cutime = std::stol(cpu_usage[2]);
+  const long int cstime = std::stol(cpu_usage[3]);
+  const long int starttime = std::stol(cpu_usage[4]);
+  const long int systime = LinuxParser::UpTime();
+
+  // Calculate cpu utilization
+  const float TOTAL_TIME =
+      (utime + stime + cutime + cstime) / sysconf(_SC_CLK_TCK);
+  const float ELAPSED_TIME = systime - (starttime / sysconf(_SC_CLK_TCK));
+  if (ELAPSED_TIME == 0.0) {
+    cpu_utilization = 0.0;
+  } else {
+    cpu_utilization = TOTAL_TIME / ELAPSED_TIME;
+  }
+}
+
+// Return the command that generated this process
+string Process::Command() { return LinuxParser::Command(this->pid); }
+
+// Return this process's memory utilization
+string Process::Ram() { return LinuxParser::Ram(this->pid); }
+
+// Return the user (name) that generated this process
+string Process::User() { return LinuxParser::User(this->pid); }
+
+// Return the uptime running of this process in seconds
+long int Process::UpTime() {
+  // std::cout << LinuxParser::UpTime(this->pid) << std::endl;
+  return LinuxParser::UpTime(this->pid);
+}
+
+// Return the disk read rate of this process
+double Process::GetDiskReadRateForPID() { return LinuxParser::GetDiskReadRateForPID(this->pid); }
+
+// Return the disk write rate of this process in seconds
+double Process::GetDiskWriteRateForPID() { return LinuxParser::GetDiskWriteRateForPID(this->pid); }
+
+// Overload the "less than" comparison operator for Process objects
+bool Process::operator<(Process const &a) const {
+  return getCpuUtilization() < a.getCpuUtilization();
+}
+
+
---
+++ src/processor.cpp
@@ -0,0 +1,75 @@
+#include "processor.h"
+#include "linux_parser.h"
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+// Returns the aggregate CPU utilization
+float Processor::Utilization() {
+  vector<string> cpu_usage = LinuxParser::CpuUtilization();
+  const long int usertime = std::stol(cpu_usage[0]);
+  const long int nicetime = std::stol(cpu_usage[1]);
+  const long int systemtime = std::stol(cpu_usage[2]);
+  const long int idletime = std::stol(cpu_usage[3]);
+  const long int iowait = std::stol(cpu_usage[4]);
+  const long int irq = std::stol(cpu_usage[5]);
+  const long int softirq = std::stol(cpu_usage[6]);
+  const long int steal = std::stol(cpu_usage[7]);
+
+  // Calculate needed jiffy values for current state
+  const float IDLE = idletime + iowait;
+  const float NON_IDLE =
+      usertime + nicetime + systemtime + irq + softirq + steal;
+  const float TOTAL = IDLE + NON_IDLE;
+
+  if (prevuser == -1 || prevnice == -1 || prevsystem == -1 || previdle == -1 ||
+      previowait == -1 || previrq == -1 || prevsoftirq == -1 ||
+      prevsteal == -1) {
+      
+    // Save current CPU status for future calculations
+    prevuser = usertime;
+    prevnice = nicetime;
+    prevsystem = systemtime;
+    previdle = idletime;
+    previowait = iowait;
+    previrq = irq;
+    prevsoftirq = softirq;
+    prevsteal = steal;
+
+    // Calculate percentage of CPU usage
+    if (TOTAL == 0.0) {
+      return 0.0;
+    }
+    return (TOTAL - IDLE) / TOTAL;
+  } else {
+    // Calculate needed jiffy values (previous state)
+    const long int PREV_IDLE = previdle + previowait;
+    const long int PREV_NON_IDLE =
+        prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal;
+    const long int PREV_TOTAL = PREV_IDLE + PREV_NON_IDLE;
+
+    // Calculate differential values
+    const float TOTAL_DIF = TOTAL - PREV_TOTAL;
+    const float IDLE_DIF = IDLE - PREV_IDLE;
+
+    // Save current CPU status for future calculations
+    prevuser = usertime;
+    prevnice = nicetime;
+    prevsystem = systemtime;
+    previdle = idletime;
+    previowait = iowait;
+    previrq = irq;
+    prevsoftirq = softirq;
+    prevsteal = steal;
+
+    // Calculate percentage of CPU usage
+    if (TOTAL_DIF == 0.0) {
+      return 0.0;
+    }
+    return (TOTAL_DIF - IDLE_DIF) / TOTAL_DIF;
+  }
+  return 0.0;
+}
